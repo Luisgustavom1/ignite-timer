fb1a9576f5607bcfb9f1fa4fd4141357c2c2fbd8
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-09-04 21:58:54 -0300

    Using zod and react hook form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 import {
   CountdownContainer,
   FormContainer,
@@ -8,10 +11,37 @@ import {
   StartCountdownButton,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa de ser no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa de ser no máximo 60 minutos'),
+  task: zod.string().min(1, 'Informe a tarefa'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 const Home = () => {
+  const { register, watch, handleSubmit, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  const handleCreateNewTask = (data: NewCycleFormData) => {
+    console.log('data', data)
+
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <FormContainer>
+      <FormContainer onSubmit={handleSubmit(handleCreateNewTask)} action="">
         <FormContent>
           <label htmlFor="task">Vou trabalhar em</label>
           <HomeInput
@@ -20,6 +50,7 @@ const Home = () => {
             isFullScreen
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -35,7 +66,9 @@ const Home = () => {
             min={5}
             max={60}
             type="number"
+            placeholder="05"
             id="minutesAmount"
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <p>minutos.</p>
@@ -49,7 +82,7 @@ const Home = () => {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
